e5c478d00c33d3bd902801d6c00aaba0cd8643f3
leo <leolima345995@gmail.com>
2022-07-24 18:10:37 -0300

    ViaCep api adicionada

--- src/components/Forms/AccountForm/index.tsx
+++ src/components/Forms/AccountForm/index.tsx
@@ -6,11 +6,11 @@ import auth from '@react-native-firebase/auth';
 import { Button } from '@components/Controllers/Button';
 import { Input } from '@components/Controllers/Input';
 import { Form, Title } from './styles';
-
+import { getAddressByCEP } from 'cep-address-finder'
 import * as yup from 'yup';
 import {Formik} from 'formik';
 import ModalDropdown from 'react-native-modal-dropdown';
-
+import { TouchableOpacity } from 'react-native-gesture-handler';
 import firestore from '@react-native-firebase/firestore';
 import { nativeViewGestureHandlerProps } from 'react-native-gesture-handler/lib/typescript/handlers/NativeViewGestureHandler';
 
@@ -21,14 +21,23 @@ export function AccountForm() {
 
   var email='';
   var senha='';
-  var CPF='';
+  var CEP='';
   var nome='';
   var sobrenome='';
   var telefone='';
   var campus='';
   const [isLoading, setIsLoading] = useState(false);
+  var [city,setcity] = useState('');
+  var [rj,setRJ]=useState(false);
+
+  //const myTimeout = setTimeout(handleNewAccount, 5000);
 
    async function handleNewAccount() {
+
+     if(rj==false){
+       Alert.alert("Você precisa checar seu CEP, e ter residência no Rio de Janeiro");
+       return;
+     }
 
       setIsLoading(true);
       const newUser = await auth().createUserWithEmailAndPassword(email, senha)
@@ -42,13 +51,24 @@ export function AccountForm() {
       nome,
       sobrenome,
       telefone,
-      CPF,
+      CEP,
       campus
     })
     .then(() => Alert.alert("Conta", "Cadastrado com sucesso!"))
     .catch((error) => console.log(error))
     .finally(() => setIsLoading(false));
 
+  }
+
+     function getcity(cep){
+
+       if(!cep.match(/^\d{5}-?\d{3}$/)) return;
+
+      getAddressByCEP(cep).then(address => {
+      setcity(address.city);;
+   });
+   if(city!="Rio de Janeiro"){setRJ(false); Alert.alert(city+" --> Você não pode frequentar a UNIRIO sem estar no Rio!")}
+   else{setRJ(true);Alert.alert(city+" --> Tudo ok!")}
    }
 
    const loginValidationSchema = yup.object().shape({
@@ -59,9 +79,9 @@ export function AccountForm() {
    senha: yup
      .string()
      .required('É necessário preencher todos os campos'),
-     cpf: yup
+     CEP: yup
        .string()
-       .matches(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF inválido"),
+       .matches(/^\d{5}-?\d{3}$/, "CEP inválido (digite apenas números)"),
      telefone: yup
        .string()
        .matches(/^[0-9]?[0-9]{4}-?[0-9]{4}$/, "Telefone inválido")
@@ -81,89 +101,101 @@ export function AccountForm() {
     <Formik
              validationSchema={loginValidationSchema}
              initialValues={{ nome: '',sobrenome:'', email: '',
-             senha: '', cpf: '', telefone: '', campus: '' }}
+             senha: '', CEP: '', telefone: '', campus: '' }}
              onSubmit={values => { email = values.email; senha = values.senha;
                nome = values.nome;sobrenome = values.sobrenome;
-               CPF = values.cpf; telefone = values.telefone;
+               CEP = values.CEP; telefone = values.telefone;
                campus = values.campus; handleNewAccount()}}
             >
              {({handleChange,handleBlur,handleSubmit,values, errors,isValid, }) => (
                <View>
-               <TextInput
-                         name="nome"
-                         placeholder="Nome"
-                         
-                         onChangeText={handleChange('nome')}
-                         onBlur={handleBlur('nome')}
-                         value={values.nome}
-                         style={style.TextInput}
-                       />
-                       {errors.nome &&
-                         <Text style={{ fontSize: 10, color: 'red' }}>{errors.nome}</Text>
-                       }
-               <TextInput
-                 name="Sobrenome"
-                 placeholder="Sobrenome"
-                 onChangeText={handleChange('sobrenome')}
-                 onBlur={handleBlur('sobrenome')}
-                 value={values.sobrenome}
-                 style={style.TextInput}
-               />
-               {errors.sobrenome &&
-                 <Text style={{ fontSize: 10, color: 'red' }}>{errors.sobrenome}</Text>
-               }
-               <TextInput
-                 name="telefone"
-                 placeholder="Digite seu telefone"
-                 onChangeText={handleChange('telefone')}
-                 onBlur={handleBlur('telefone')}
-                 value={values.telefone}
-                 style={style.TextInput}
-               />
-               {errors.telefone &&
-                 <Text style={{ fontSize: 10, color: 'red' }}>{errors.telefone}</Text>
-               }
-               <TextInput
-                 name="cpf"
-                 placeholder="Digite seu CPF"
-                 onChangeText={handleChange('cpf')}
-                 onBlur={handleBlur('cpf')}
-                 value={values.cpf}
-                 style={style.TextInput}
-               />
-               {errors.cpf &&
-                 <Text style={{ fontSize: 10, color: 'red' }}>{errors.cpf}</Text>
-               }
-                 <TextInput
-                   name="email"
-                   placeholder="Endereço de email"
-                   onChangeText={handleChange('email')}
-                   onBlur={handleBlur('email')}
-                   value={values.email}
-                   style={style.TextInput}
-                 />
-                 {errors.email &&
-                   <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
-                 }
-                 <TextInput
-                   name="senha"
-                   placeholder="Senha"
-                   onChangeText={handleChange('senha')}
-                   onBlur={handleBlur('senha')}
-                   value={values.senha}
-                   secureTextEntry
-                   style={style.TextInput}
-                 />
-                 {errors.senha &&
-                   <Text style={{ fontSize: 10, color: 'red' }}>{errors.senha}</Text>
-                 }
+                   <TextInput
+                             name="nome"
+                             placeholder="Nome"
 
-                 <Button
-                   title="Cadastrar"
-                   isLoading={isLoading}
-                   onPress={handleSubmit}
-                   disabled={!isValid}
-                 />
+                             onChangeText={handleChange('nome')}
+                             onBlur={handleBlur('nome')}
+                             value={values.nome}
+                             style={style.TextInput}
+                           />
+                           {errors.nome &&
+                             <Text style={{ fontSize: 10, color: 'red' }}>{errors.nome}</Text>
+                           }
+                   <TextInput
+                     name="Sobrenome"
+                     placeholder="Sobrenome"
+                     onChangeText={handleChange('sobrenome')}
+                     onBlur={handleBlur('sobrenome')}
+                     value={values.sobrenome}
+                     style={style.TextInput}
+                   />
+                   {errors.sobrenome &&
+                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.sobrenome}</Text>
+                   }
+                   <TextInput
+                     name="telefone"
+                     placeholder="Digite seu telefone"
+                     onChangeText={handleChange('telefone')}
+                     onBlur={handleBlur('telefone')}
+                     value={values.telefone}
+                     style={style.TextInput}
+                   />
+                   {errors.telefone &&
+                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.telefone}</Text>
+                   }
+
+                   <TextInput
+                     name="CEP"
+                     placeholder="Digite seu CEP"
+                     onChangeText={handleChange('CEP')}
+                     onBlur={handleBlur('CEP')}
+                     value={values.CEP}
+                    style={style.TextInput}
+                   />
+
+                   {errors.CEP &&
+                     <Text style={{ fontSize: 10, color: 'red' }}>{errors.CEP}</Text>
+                   }
+
+                       <TouchableOpacity style={{paddingLeft:'2%'}} onPress={()=> getcity(values.CEP)}>
+                       <View style={{backgroundColor:'#008000',borderRadius: 30,marginRight:'72%'}}>
+                           <Text
+                             style={{paddingLeft: 10, fontSize: 15, fontWeight: 'bold',color: '#FFFFFF'}}>
+                             Checar CEP
+                           </Text>
+                         </View>
+                         </TouchableOpacity>
+
+                     <TextInput
+                       name="email"
+                       placeholder="Endereço de email"
+                       onChangeText={handleChange('email')}
+                       onBlur={handleBlur('email')}
+                       value={values.email}
+                       style={style.TextInput}
+                     />
+                     {errors.email &&
+                       <Text style={{ fontSize: 10, color: 'red' }}>{errors.email}</Text>
+                     }
+                     <TextInput
+                       name="senha"
+                       placeholder="Senha"
+                       onChangeText={handleChange('senha')}
+                       onBlur={handleBlur('senha')}
+                       value={values.senha}
+                       secureTextEntry
+                       style={style.TextInput}
+                     />
+                     {errors.senha &&
+                       <Text style={{ fontSize: 10, color: 'red' }}>{errors.senha}</Text>
+                     }
+
+                     <Button
+                       title="Cadastrar"
+                       isLoading={isLoading}
+                       onPress={handleSubmit}
+                       disabled={!isValid}
+                     />
               </View>
              )}
       </Formik>
@@ -175,6 +207,9 @@ export function AccountForm() {
       width: '80%',
       alignContent: 'center',
       paddingLeft: '15%'
+    },
+    CEP: {
+      flexDirection:'row'
     },
 
     TextInput: {
